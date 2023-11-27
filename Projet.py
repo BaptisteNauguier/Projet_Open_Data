b1fca3f88b6c8c4dd464c678fdd4dcaf79bdb2f7
Baptiste Nauguier <bnauguier@gmail.com>
2023-11-27 16:22:37 +0100

    ajout onglet 1 et 4

--- Projet.py
+++ Projet.py
@@ -28,19 +28,15 @@ with tab1:
         Cette application offre une analyse approfondie des tendances globales du cancer et des décès associés de 1990 à 2019. 
         Elle est structurée en plusieurs sections, chacune accessible via des onglets :
 
-        **1. Page d'Accueil**
-        - **Titre** : "Analyse Globale des Tendances du Cancer et des Décès (1990-2019)"
-        - **Description** : Une introduction sur l'objectif de l'application et un résumé des données disponibles.
-
-        **2. Carte Interactive Mondiale**
+        **Onglet 2 : Carte Interactive Mondiale**
         - **Fonctionnalités** : Affichage des taux de cancer et de mortalité par pays pour l'année sélectionnée.
         - **Interactivité** : Possibilité de survoler les pays pour afficher des statistiques détaillées, telles que le nombre de cas de cancer et le taux de mortalité.
 
-        **3. Analyse par Région**
+        **Onglet 3 : Analyse par Région**
         - **Sélection** : Choix de différentes régions (Amérique, Europe, Asie, etc.) pour des visualisations et analyses spécifiques.
         - **Visualisations** : Graphiques et tableaux récapitulatifs pour une comparaison claire entre les régions.
 
-        **4. Analyse Approfondie par Pays**
+        **Onglet 4 : Analyse Approfondie par Pays**
         - **Exploration** : Corrélations entre les taux de cancer et divers facteurs socio-économiques comme le PIB par habitant, le niveau de vie, et l'accès aux soins de santé.
         - **Graphiques Interactifs** : Outils de visualisation interactifs pour examiner ces corrélations en détail et explorer les tendances dans le temps et par pays.
         
@@ -75,6 +71,8 @@ with tab1:
         df_selected = df_share_of_population_with_cancer_by_age
     elif dataset_name == "08 - Taux de charge de la maladie par types de cancer":
         df_selected = df_disease_burden_rates_by_cancer_types
+    elif dataset_name == "09 - Taux de mortalité par cancer et indice standardisé selon l'âge":
+        df_selected = df_cancer_deaths_rate_and_age_standardized_rate_index
 
 # Filtres basés sur les colonnes du jeu de données sélectionné
     if not df_selected.empty:
@@ -83,6 +81,25 @@ with tab1:
         if 'Year' in df_selected.columns:
             years = st.multiselect('Année', df_selected['Year'].unique())
             df_selected = df_selected[df_selected['Year'].isin(years)] if years else df_selected
+
+         # Calculer le pourcentage de valeurs remplies et nulles pour chaque colonne
+        percent_filled = df_selected.notna().mean() * 100
+        percent_na = df_selected.isna().mean() * 100
+        info_columns = pd.DataFrame({
+            'Pourcentage Rempli (%)': percent_filled.round(2),
+            'Pourcentage NA (%)': percent_na.round(2),
+            'Valeur la Plus Fréquente': df_selected.apply(lambda x: x.value_counts().index[0] if not x.empty else None),
+            'Moyenne': df_selected.mean(),
+            'Médiane': df_selected.median(),
+            'Écart-Type': df_selected.std(),
+            'Min': df_selected.min(),
+            'Max': df_selected.max()
+        }).transpose()
+
+
+        # Afficher les informations sur les colonnes
+        st.write("Informations sur les colonnes :")
+        st.dataframe(info_columns)
 
         # Afficher les données filtrées
         st.write("Aperçu des données filtrées :")
@@ -98,7 +115,41 @@ with tab3:
 
 with tab4:
     st.header("Analyse Approfondie par Pays")
-    # Code pour l'analyse approfondie par pays
+
+    # Étape 1 : Choix du fichier CSV
+    dataset_name = st.selectbox(
+        "Choisissez un jeu de données pour l'ACP",
+        ("02 total-cancer-deaths-by-type.csv", "03 cancer-death-rates-by-age.csv", "05 share-of-population-with-cancer.csv", "06 number-of-people-with-cancer-by-age.csv", "07 share-of-population-with-cancer-by-age.csv", "08 disease-burden-rates-by-cancer-types.csv", "09 cancer-deaths-rate-and-age-standardized-rate-index.csv")
+    )
+
+    # Étape 2 : Chargement des données CSV
+    df = pd.read_csv(data_path + dataset_name)
+
+    # Vérifiez si 'Entity' est dans le DataFrame et définissez-le comme index si nécessaire
+    if 'Entity' in df.columns:
+        df = df.set_index('Entity')
+    
+    # Gardez uniquement les colonnes numériques pour l'ACP
+    df_numeric = df.select_dtypes(include=[np.number])
+
+    # Si une colonne 'Country' est présente, l'utiliser comme index
+    if 'Entity' in df.columns:
+        df_numeric = df_numeric.set_index(df['Entity'])
+        df_numeric.drop('Entity', axis=1, inplace=True)
+
+    # Étape 3 : Appliquer l'ACP
+    pca = PCA(n_components=2)  # Réduction à 2 dimensions
+    pca_result = pca.fit_transform(df_numeric.values)
+
+    # Créer un DataFrame avec les résultats de l'ACP
+    pca_df = pd.DataFrame(pca_result, columns=['PC1', 'PC2'], index=df_numeric.index)
+
+    # Étape 4 : Afficher le résultat de l'ACP dans un graphique
+    fig = px.scatter(pca_df, x='PC1', y='PC2', text=df_numeric.index)
+    fig.update_traces(textposition='top center')
+    fig.update_layout(height=600, width=800)
+
+    st.plotly_chart(fig)
 
 
 # Fonction principale de l'app
